ce6367529876a2f1b62eefe6d2897625babf69d6
Scott Nelson <scott@thisbythem.com>
2011-09-01 13:12:42 -0700

    Init new Rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParallelIssue::Application.config.secret_token = 'e0512bf719699bef495add3ec22feeb8eebe89dee0b8c3b392689f48a6f7ff700f53ea68ea716722557525700d6874b55f26acd0a48a7beec7b33380dea90023'
