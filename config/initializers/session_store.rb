1eccea0f13cd65b4a34e472eebe770d183a7c2df
Scott Nelson <scott@thisbythem.com>
2011-09-02 19:53:39 -0700

    1.8.7 hash fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ParallelIssue::Application.config.session_store :cookie_store, key: '_parallel_issue_session'
+ParallelIssue::Application.config.session_store :cookie_store, :key => '_parallel_issue_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
